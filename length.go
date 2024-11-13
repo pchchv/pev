18b674bb3ce82e8b6a1430641dfa891356f034cb
pchchv <ipchchv@gmail.com>
2024-11-13 12:59:39 +0300

    Implement getReversedString and getLength funcs

--- length.go
+++ length.go
@@ -1,5 +1,13 @@
 package pev
 
+const (
+	seqNums      = "0123456789"
+	seqKeyboard0 = "qwertyuiop"
+	seqKeyboard1 = "asdfghjkl"
+	seqKeyboard2 = "zxcvbnm"
+	seqAlphabet  = "abcdefghijklmnopqrstuvwxyz"
+)
+
 func deleteRuneAt(runes []rune, i int) []rune {
 	if i >= len(runes) ||
 		i < 0 {
@@ -56,4 +64,38 @@ func removeMoreThanTwoRepeatingChars(s string) string {
 	}
 
 	return string(runes)
+}
+
+func getReversedString(s string) string {
+	var n int
+	rune := make([]rune, len(s))
+	for _, r := range s {
+		rune[n] = r
+		n++
+	}
+
+	rune = rune[0:n]
+	// Reverse
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+
+	// Convert back to UTF-8.
+	return string(rune)
+}
+
+func getLength(password string) int {
+	password = removeMoreThanTwoRepeatingChars(password)
+	password = removeMoreThanTwoFromSequence(password, seqNums)
+	password = removeMoreThanTwoFromSequence(password, seqKeyboard0)
+	password = removeMoreThanTwoFromSequence(password, seqKeyboard1)
+	password = removeMoreThanTwoFromSequence(password, seqKeyboard2)
+	password = removeMoreThanTwoFromSequence(password, seqAlphabet)
+	password = removeMoreThanTwoFromSequence(password, getReversedString(seqNums))
+	password = removeMoreThanTwoFromSequence(password, getReversedString(seqKeyboard0))
+	password = removeMoreThanTwoFromSequence(password, getReversedString(seqKeyboard1))
+	password = removeMoreThanTwoFromSequence(password, getReversedString(seqKeyboard2))
+	password = removeMoreThanTwoFromSequence(password, getReversedString(seqAlphabet))
+
+	return len(password)
 }
