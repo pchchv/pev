fe3dbb3c3550dd61c005f912e70a11858c54ee16
pchchv <ipchchv@gmail.com>
2024-11-13 13:28:26 +0300

    Fix

--- entropy_test.go
+++ entropy_test.go
@@ -1,33 +1,31 @@
-package pev_test
+package pev
 
 import (
 	"math"
 	"testing"
-
-	"github.com/pchchv/pev"
 )
 
 func TestLogPow(t *testing.T) {
 	expected := math.Round(math.Log2(math.Pow(7, 8)))
-	actual := math.Round(pev.logPow(7, 8, 2))
+	actual := math.Round(logPow(7, 8, 2))
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
 
 	expected = math.Round(math.Log2(math.Pow(10, 11)))
-	actual = math.Round(pev.logPow(10, 11, 2))
+	actual = math.Round(logPow(10, 11, 2))
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
 
 	expected = math.Round(math.Log2(math.Pow(11, 17)))
-	actual = math.Round(pev.logPow(11, 17, 2))
+	actual = math.Round(logPow(11, 17, 2))
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
 
 	expected = math.Round(math.Log10(math.Pow(13, 21)))
-	actual = math.Round(pev.logPow(13, 21, 10))
+	actual = math.Round(logPow(13, 21, 10))
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
